8956ea0a3edb808764ed6f8dcaa664d92ec0ea04
fdezruizdavid <93644070+davidfr5@users.noreply.github.com>
2025-05-21 18:09:54 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -12,12 +12,23 @@ class User(db.Model):
     password = db.Column(String(100), nullable=False)
     first_name = db.Column(String(50))
     last_name = db.Column(String(50))
-    subscription_date = db.Column(DateTime, default=datetime.datetime)
+    subscription_date = db.Column(DateTime, default=datetime.datetime.utcnow)
     is_active = db.Column(Boolean(), default=True)
 
-    favorite_characters = relationship("FavoriteCharacter", back_populates="user")
     favorite_planets = relationship("FavoritePlanet", back_populates="user")
+    favorite_characters = relationship("FavoriteCharacter", back_populates="user")
 
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "subscription_date": self.subscription_date.isoformat()
+        }
 
 class Planet(db.Model):
     __tablename__ = 'planet'
@@ -27,7 +38,7 @@ class Planet(db.Model):
     climate = db.Column(String(50))
     terrain = db.Column(String(50))
 
-    favorites = relationship("Favorite", back_populates="planet")
+    favorite_planets = relationship("FavoritePlanet", back_populates="planet")
 
     def __repr__(self):
         return f'<Planet {self.name}>'
@@ -49,7 +60,7 @@ class Character(db.Model):
     birth_year = db.Column(String(20))
     eye_color = db.Column(String(20))
 
-    favorites = relationship("Favorite", back_populates="character")
+    favorite_characters = relationship("FavoriteCharacter", back_populates="character")
 
     def __repr__(self):
         return f'<Character {self.name}>'
@@ -63,30 +74,17 @@ class Character(db.Model):
             "eye_color": self.eye_color
         }
 
-class FavoriteCharacter(db.Model):
-    __tablename__ = 'favorite_character'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
-
-    user = relationship("User", back_populates="favorite_characters")
-    character = relationship("Character")
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "character": self.character.serialize()
-        }
-
 class FavoritePlanet(db.Model):
     __tablename__ = 'favorite_planet'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, ForeignKey('planet.id'), nullable=False)
 
     user = relationship("User", back_populates="favorite_planets")
-    planet = relationship("Planet")
+    planet = relationship("Planet", back_populates="favorite_planets")
+
+    def __repr__(self):
+        return f'<FavoritePlanet {self.id} - User {self.user_id} - Planet {self.planet_id}>'
 
     def serialize(self):
         return {
@@ -95,3 +93,21 @@ class FavoritePlanet(db.Model):
             "planet": self.planet.serialize()
         }
 
+class FavoriteCharacter(db.Model):
+    __tablename__ = 'favorite_character'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    character_id = db.Column(db.Integer, ForeignKey('character.id'), nullable=False)
+
+    user = relationship("User", back_populates="favorite_characters")
+    character = relationship("Character", back_populates="favorite_characters")
+
+    def __repr__(self):
+        return f'<FavoriteCharacter {self.id} - User {self.user_id} - Character {self.character_id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character": self.character.serialize()
+        }
